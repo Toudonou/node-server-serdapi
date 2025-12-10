e44efe0bbaccffb2afd5aa99fc242e734b4513a3
Dona MEDENOU <79231480+Daryl-03@users.noreply.github.com>
2025-12-10 15:00:00 +0100

    Refactor server port configuration and dotenv usage

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config({path: ".env"});
+require('dotenv').config();
 
 const { createServer } = require('node:http');
 const { getJson } = require("serpapi");
 
 const hostname = '127.0.0.1';
-const port = 3050;
+const port = process.env.PORT || 3050;
 
 const getJsonAsync = (params) => {
     return new Promise((resolve, reject) => {
@@ -60,6 +60,6 @@ const server = createServer(async (req, res) => {
     }
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
